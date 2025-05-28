9610ea9175ccc470a26d05a213a541445b83506f
Mohit Sharma <161517182+shrmohit@users.noreply.github.com>
2025-05-28 12:03:36 +0530

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -15,7 +15,7 @@ export const sighUp=async (req,res) => {
         res.cookie("token",token,{
             httpOnly:true,
             secure:true,
-            sameSite: "n",
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
 
 
