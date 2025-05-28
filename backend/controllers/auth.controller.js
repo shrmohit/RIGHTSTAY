0e9befd0ad289d09ac9baee24955e8d7c58294ea
Mohit Sharma <161517182+shrmohit@users.noreply.github.com>
2025-05-28 12:02:13 +0530

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -14,8 +14,8 @@ export const sighUp=async (req,res) => {
         let token = await genToken(user._id)
         res.cookie("token",token,{
             httpOnly:true,
-            secure:process.env.NODE_ENVIRONMENT = "production",
-            sameSite: "strict",
+            secure:true,
+            sameSite: "n",
             maxAge: 7 * 24 * 60 * 60 * 1000
 
 
